b1bffccbee1ca3868345121185ec051435de6a9c
CLOUDI <96988872+badal-pundir@users.noreply.github.com>
2024-06-17 13:39:59 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodeJs, not brower. It will work in console in browser.
+
+console.log(3 + 3)
+// string
+let name = "badal" // 'badal' also wrok
+// number: Int and bigInt
+let age = 22
+// Boolean: true/false
+let isLoggedIn = false
+// null: standalone value
+let temperature = null 
+// undefined
+let state // ; in end will also wrok
+//symbol: to find the uniqueness component
+
+
+//object
+
+console.log(typeof age) // number
+console.log(typeof "badal") // string
+console.log(typeof temperature) // Object
+console.log(typeof state)   // undefined
+
