bb5234971fec31c7b76284e30ecfe9f7e5f2e85d
CLOUDI <96988872+badal-pundir@users.noreply.github.com>
2024-06-22 17:16:56 +0000

    min max concept done

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -18,3 +18,19 @@ console.log(Number.MAX_VALUE)
 console.log(Number.MAX_SAFE_INTEGER)
 console.log(Number.MIN_VALUE)
 
+// ++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++
+
+console.log(Math);  // Object [Maths]
+console.log(Math.abs(-4));
+console.log(Math.round(4.6))    // 5
+console.log(Math.ceil(4.6))     // 5
+console.log(Math.floor(4.6))    // 4
+console.log(Math.max(2, 3, 4, 6))
+console.log(Math.random())     // [0, 1]
+console.log((Math.random()*10) + 1)     // 1 to 9
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
