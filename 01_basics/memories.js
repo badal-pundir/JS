4e677ce3ad7692ec8997e02a4ad453bf16d25f18
CLOUDI <96988872+badal-pundir@users.noreply.github.com>
2024-09-20 11:15:03 +0000

    memories updated

--- 01_basics/memories.js
+++ 01_basics/memories.js
@@ -9,14 +9,18 @@ anothername = "Cloudy"
 console.log(myName)
 console.log(anothername)
 
-// non primitive type
+// non primitive type, this will store in heap.
 userOne = {
     email: "user@google.com",
     uip : "user@ybl"
 }
 
 let userTwo = userOne   // points to the same memory location of the userOne object.
-
+// Before: 
+console.log("user one's email: " + userOne.email)
+// we are changing the email of user two
 userTwo.email = "badalpundir@gooogle.com"
-console.log(userOne.email)
-console.log(userTwo.email)
+// user one's email will also get change coz both point to same location.
+// After:
+console.log("user one's email: " + userOne.email)
+console.log("user two's email: " + userTwo.email)
